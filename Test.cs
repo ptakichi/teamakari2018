1920a4116c49ebf8c8485925fc53099c499cedc4
本多 裕幸 <p.takichi60@gmail.com>
2018-11-04 17:37:47 +0900

    Test.cs追加

---
+++ Test.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace teamakari2018
+{
+    public class Test
+    {
+
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
