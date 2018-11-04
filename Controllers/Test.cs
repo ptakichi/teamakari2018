e15353ce6706197fe1746e223e76de21020f5c21
本多 裕幸 <p.takichi60@gmail.com>
2018-11-04 18:43:23 +0900

    Controller変更

--- Controllers/Test.cs
+++ Controllers/Test.cs
@@ -2,8 +2,11 @@
 
 namespace teamakari2018
 {
-    public class Test
+    [Route("api/[controller]")]
+    public class Test: Controller    
     {
+        // GET api/CalendarInfo/20171111
+        [HttpGet("{Value}")]        
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
