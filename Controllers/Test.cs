7ca418024702925ee4ee585237e7197028f0a92d
本多 裕幸 <p.takichi60@gmail.com>
2018-11-05 21:33:17 +0900

    Ignore設定

---
+++ Controllers/Test.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace teamakari2018
+{
+    [Route("api/[controller]")]
+    public class Test: Controller    
+    {
+        // GET api/Test/Value
+        [HttpGet("{Value}")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+    }
+}
