7ff989c23430369b501ab724da0e84799654b4a8
unknown <h_honta@fcs.co.jp>
2018-11-10 16:54:25 +0900

    ファイルアップロード機能追加 @大越

--- Pages/Upload.chtml.cs
+++ Pages/Upload.chtml.cs
@@ -5,18 +5,37 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
+using System.Web;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+
+
 namespace teamakari2018.Pages
 {
     public class UploadModel : PageModel
     {
         public void OnGet()
         {
-
+            
         }
-        public void OnPost()
-        {
 
+    
+    [HttpPost]
+        public void OnPost(IFormFile uploadFile){
+            if (uploadFile != null) {
+                Console.Write("\n\n\n\n\n\n\n==================\n");
+                Console.Write("FileName: {0}",uploadFile.FileName);//Gets the file name from the Content-Disposition header.
+                Console.Write("\nHeader: {0}",uploadFile.Headers);//Gets the header dictionary of the uploaded file
+                Console.Write("\nLength: {0}",uploadFile.Length);//Gets the file length in bytes.
+                Console.Write("\nName: {0}",uploadFile.Name);//Gets the form field name from the Content-Disposition header.
+                Console.Write("\nContent-type: {0}",uploadFile.ContentType);//Gets the raw Content-Type header of the uploaded file.
+                Console.Write("\n==================\n\n\n\n\n\n");
+
+            }
         }
+
+
 
     }
 }
